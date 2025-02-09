0e6e51208b2737fa49c150bceb23980c1dc14a54
mety0r <72600922+mety0r@users.noreply.github.com>
2025-02-10 03:43:04 +0530

    Translatable text

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,8 +1,9 @@
 sap.ui.require([
     "sap/ui/core/mvc/Controller",
     "sap/m/MessageToast",
-    "sap/ui/model/json/JSONModel"
-], function (Controller, MessageToast, JSONModel) {
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/model/resource/ResourceModel"
+], function (Controller, MessageToast, JSONModel, ResourceModel) {
     "use strict";
 
     return Controller.extend("my.app.namespace.controller.App", {
@@ -14,18 +15,30 @@ sap.ui.require([
                 greeting: "Hello UI5"
             };
             var OModel = new JSONModel(Odata);
-            this.getView().setModel(OModel);
+            this.getView().setModel(OModel);  
 
-            
+            // Set i18n model on view
+            var i18nModel = new ResourceModel({
+                bundleName: "my.app.namespace.i18n.i18n",
+                supportedLocales: [""],
+                fallbackLocale: ""
+            });
+            this.getView().setModel(i18nModel, "i18n");  
+
             OModel.attachPropertyChange(function (oEvent) {
                 var oData = OModel.getData();
                 oData.greeting = "Hello " + (oData.recipient.name || "");
-                OModel.refresh(true);  
+                OModel.refresh(true);
             });
         },
 
         onShowHello: function () {
-            MessageToast.show("Hello there");
+            // Read message from i18n model
+            var oBundle = this.getView().getModel("i18n").getResourceBundle();
+            var sRecipient = this.getView().getModel().getProperty("/recipient/name");
+            var sMsg = oBundle.getText("helloMsg", [sRecipient]);  
+
+            MessageToast.show(sMsg);
         }
     });
 });
