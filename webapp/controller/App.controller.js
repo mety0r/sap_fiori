d282af1c03a00d9f88d67c7f0e77c2b0fc32c6b6
mety0r <72600922+mety0r@users.noreply.github.com>
2025-02-10 03:17:54 +0530

    JSON Model

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,13 +1,31 @@
 sap.ui.require([
     "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast"
-], function (Controller, MessageToast){
-    "use strict"
-    return Controller.extend("my.app.namespace.controller.App",{
-        onShowHello: function () {
-            //show a vanilla alert
-            MessageToast.show("Hello there")
-        }
+    "sap/m/MessageToast",
+    "sap/ui/model/json/JSONModel"
+], function (Controller, MessageToast, JSONModel) {
+    "use strict";
 
+    return Controller.extend("my.app.namespace.controller.App", {
+        onInit: function () {
+            var Odata = {
+                recipient: {
+                    name: "UI5"
+                },
+                greeting: "Hello UI5"
+            };
+            var OModel = new JSONModel(Odata);
+            this.getView().setModel(OModel);
+
+            
+            OModel.attachPropertyChange(function (oEvent) {
+                var oData = OModel.getData();
+                oData.greeting = "Hello " + (oData.recipient.name || "");
+                OModel.refresh(true);  
+            });
+        },
+
+        onShowHello: function () {
+            MessageToast.show("Hello there");
+        }
     });
 });
