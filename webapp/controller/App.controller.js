11207e6419c5f2fdc6bdf3c9ab8b6a0599546885
mety0r <72600922+mety0r@users.noreply.github.com>
2025-02-10 02:44:03 +0530

    Adding MessageToast

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,11 +1,12 @@
 sap.ui.require([
-    "sap/ui/core/mvc/Controller"
-], function (Controller){
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast"
+], function (Controller, MessageToast){
     "use strict"
     return Controller.extend("my.app.namespace.controller.App",{
         onShowHello: function () {
             //show a vanilla alert
-            alert("Hello There!")
+            MessageToast.show("Hello there")
         }
 
     });
