3b341ef6dbae74668a89a74468f59837e00706fb
mety0r <72600922+mety0r@users.noreply.github.com>
2025-03-19 09:42:27 +0530

    Using Openui5 control

--- webapp/index.js
+++ webapp/index.js
@@ -1,4 +1,10 @@
-sap.ui.define([], () => {
-	"use strict";
-	alert("UI5 is ready");
+sap.ui.define([
+    "sap/m/Text"
+], (Text) => {
+    "use strict";
+    
+    new Text({
+        text: "Hello World"
+
+    }).placeAt("content")
 });
